eed6a08c45f47ff5e01d456991153b19b4e8d173
Travis <teakopp@github.com>
2018-11-24 14:33:08 -0800

    fixed main screen styling

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TabBarIOS } from 'react-native';
+import Card from './components/Card'
+import Top from './components/Top'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Currently in progress...</Text>
+      <View style={style.container}>
+      <Text style={style.role}>Inspector</Text>
+      <FlatList
+        style={style.list}
+        data={[
+          {key: 'Travis'},
+          {key: 'Molly'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Card name={item.key}></Card>}
+      />
+
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  list: {
+      paddingTop: 40,
+
+      flex: 0,
+      width: '100%',
+      textAlign: 'center',
+  },
+  role: {
+    paddingTop: 50,
+    fontSize: 44,
+    color: "white",
+    backgroundColor: "black",
+    width: '100%',
+    textAlign: 'center',
+  }
 });
